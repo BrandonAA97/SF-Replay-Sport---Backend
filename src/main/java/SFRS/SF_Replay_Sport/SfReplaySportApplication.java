5a65e242d4168d7ca495fd24a98c166b1e446224
LucasBebek <bebeklucas7@gmail.com>
2023-09-06 15:15:49 -0300

    categorias y algunos usuarios inicializados

--- src/main/java/SFRS/SF_Replay_Sport/SfReplaySportApplication.java
+++ src/main/java/SFRS/SF_Replay_Sport/SfReplaySportApplication.java
@@ -6,14 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import SFRS.SF_Replay_Sport.models.entity.Categoria;
+import SFRS.SF_Replay_Sport.models.entity.Usuario;
 import SFRS.SF_Replay_Sport.repositories.CategoriaRepo;
+import SFRS.SF_Replay_Sport.repositories.UsuarioRepo;
 
 @SpringBootApplication
 public class SfReplaySportApplication{
 
 	
-	
-			   public static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(SfReplaySportApplication.class, args);
 		
 		
@@ -32,9 +33,24 @@ public class SfReplaySportApplication{
 			Categoria categoria3 = new Categoria(3L, "Voley");
             categoriaRepo.save(categoria3);
 
+       
+		};
+       }
 
+	   @Bean
+    public CommandLineRunner initData2(UsuarioRepo usuarioRepo) {
+        return args -> {
+            // Crear registros iniciales aquí
+            Usuario usuario1 = new Usuario(1L, "zNiwel", "Gauna", "niwel123@hotmail.com");
+            usuarioRepo.save(usuario1);
 
-        };
+			Usuario usuario2 = new Usuario(2L, "Brock", "Albornoz", "brandon435@gmail.com");
+            usuarioRepo.save(usuario2);
+			
+			
+       
+
+       };
 
 }
 }
