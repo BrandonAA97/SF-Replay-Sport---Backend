9b9769fd861424b461d5594c49e1386c07ca0512
Brandon Albornoz <brandonalborno97@gmail.com>
2023-11-02 18:20:28 -0300

    fasf

--- src/main/java/SFRS/SF_Replay_Sport/auth/Jwt/JwtAuthenticationFilter.java
+++ src/main/java/SFRS/SF_Replay_Sport/auth/Jwt/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,8 +16,12 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
     private final JWTService jwtService;
     private final UserDetailsService userDetailsService;
 
